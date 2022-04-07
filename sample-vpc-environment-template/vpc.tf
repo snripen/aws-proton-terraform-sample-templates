cbe05f47deb9b769dcc79d71acb1b6bcaaaa22c8
sbx_user1051 <sbx_user1051@169.254.198.169>
2022-04-07 14:05:54 +0000

    Update requested for Proton - environment/sample-vpc-environment-template

--- sample-vpc-environment-template/vpc.tf
+++ sample-vpc-environment-template/vpc.tf
@@ -17,7 +17,16 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = false
+  enable_vpn_gateway = true
+  
+  enable_ipv6                     = true
+  assign_ipv6_address_on_creation = true
+
+  private_subnet_assign_ipv6_address_on_creation = false
+
+  public_subnet_ipv6_prefixes   = [0, 1]
+  private_subnet_ipv6_prefixes  = [2, 3]
+  database_subnet_ipv6_prefixes = [4, 5]
 
   tags = {
     Terraform   = "true"
