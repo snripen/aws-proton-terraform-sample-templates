70d476b7814a432d8516bbdebdc007ebc3717887
warpspeed6 <25733040+warpspeed6@users.noreply.github.com>
2022-04-07 17:18:02 +0200

    Update vpc.tf

--- lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
+++ lambda-vpc/sample-vpc-environment-template/v1/infrastructure/vpc.tf
@@ -5,16 +5,16 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.aws_region}a"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway                             = true
   enable_vpn_gateway                             = true
   enable_ipv6                                    = true
   assign_ipv6_address_on_creation                = true
   private_subnet_assign_ipv6_address_on_creation = false
-  public_subnet_ipv6_prefixes                    = [0, 1]
-  private_subnet_ipv6_prefixes                   = [2, 3]
+  public_subnet_ipv6_prefixes                    = [0]
+  private_subnet_ipv6_prefixes                   = [2]
 
   tags = {
     Terraform   = "true"
