fe3c89eb0ebe2e9839813f1ff7b5988a27b35084
warpspeed6 <25733040+warpspeed6@users.noreply.github.com>
2022-04-12 15:31:05 +0200

    Update lambda.tf

--- lambda-vpc/sample-lambda-function-template/v1/instance_infrastructure/lambda.tf
+++ lambda-vpc/sample-lambda-function-template/v1/instance_infrastructure/lambda.tf
@@ -20,15 +20,19 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name = var.service_instance.inputs.function_name
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.service_instance.inputs.handler
+data "archive_file" "lambda_zip" {
+    type          = "zip"
+    source_file   = "index.js"
+    output_path   = "lambda_function.zip"
+}
 
-  s3_bucket = var.service_instance.inputs.function_s3_bucket
-  s3_key    = var.service_instance.inputs.function_s3_key
-
-  runtime = var.service_instance.inputs.lambda_runtime
+resource "aws_lambda_function" "lambda_service" {
+  function_name     = var.service_instance.inputs.function_name
+  filename          = "lambda_function.zip"
+  role              = aws_iam_role.iam_for_lambda.arn
+  source_code_hash  = "${data.archive_file.lambda_zip.output_base64sha256}"
+  runtime           = "nodejs6.10"
+  handler           = "index.handler"
 
   vpc_config {
     subnet_ids         = [var.environment.outputs.subnet_id]
