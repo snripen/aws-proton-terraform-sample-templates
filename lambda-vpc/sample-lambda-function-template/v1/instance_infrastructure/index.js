2fc347d533bd7fa3ba56fd807232984b3057eab4
warpspeed6 <25733040+warpspeed6@users.noreply.github.com>
2022-04-12 15:33:41 +0200

    Update index.js

--- lambda-vpc/sample-lambda-function-template/v1/instance_infrastructure/index.js
+++ lambda-vpc/sample-lambda-function-template/v1/instance_infrastructure/index.js
@@ -1,5 +1,5 @@
 // 'Hello World' nodejs6.10 runtime AWS Lambda function
 exports.handler = (event, context, callback) => {
-    console.log('Hello, logs!');
+    console.log('Hello There, logs!');
     callback(null, 'great success');
 }
